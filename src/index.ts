40f2428d01ea2044b071859c708f6df3c06bd21d
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-16 11:03:31 -0300

    [feat] index - instantiate app module

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-import express from 'express';
+import express, { Router } from 'express';
 import mongoose from 'mongoose';
+import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
+const appModule = new AppModule();
+export const router = Router();
 
 mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
-    console.log('\x1b[42m--MONGO CONNECTED\x1b[0m');
+    const app = express();
+    const port = 3001;
 
-    app.listen(3001, () => {
-      console.log('\x1b[44m--SERVER RUNNING\x1b[0m');
+    app.use(express.json());
+    app.use(router);
+
+    app.listen(port, () => {
+      console.log('\x1b[42m--SERVER RUNNING\x1b[0m');
     });
+
+    appModule.instantiate();
   })
-  .catch(() => console.log('MONGO CONNECTION ERROR'));
+  .catch((err) => console.log('\x1b[41m--MONGO CONNECTION ERROR\x1b[0m', err));
