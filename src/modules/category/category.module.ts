717e6c7e64f9c466859cd7b034dbc351d63356da
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-16 11:02:39 -0300

    [feat] add category module

---
+++ src/modules/category/category.module.ts
@@ -0,0 +1,11 @@
+import { CategoryController } from './category.controller';
+import { CategoryService } from './category.service';
+
+export class CategoryModule {
+  instantiate() {
+    const controller = new CategoryController(new CategoryService());
+
+    controller.createCategory();
+    controller.listCategories();
+  }
+}
