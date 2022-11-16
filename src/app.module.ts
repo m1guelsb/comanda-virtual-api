f6be72197f9712d7a7dc3a17154c0a85da83ae4b
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-16 20:09:33 -0300

    [feat] app module - instantiate product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { CategoryModule } from './modules/category/category.module';
+import { ProductModule } from './modules/product/product.module';
 
 export class AppModule {
   instantiate() {
     new CategoryModule().instantiate();
+    new ProductModule().instantiate();
   }
 }
