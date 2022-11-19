9605e0cfe5e6283360f61d9feaa1894130a4cdc2
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-18 23:37:25 -0300

    [feat] AppModule - add order module instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { CategoryModule } from './modules/category/category.module';
+import { OrderModule } from './modules/order/order.module';
 import { ProductModule } from './modules/product/product.module';
 
 export class AppModule {
   instantiate() {
     new CategoryModule().instantiate();
+    new OrderModule().instantiate();
     new ProductModule().instantiate();
   }
 }
