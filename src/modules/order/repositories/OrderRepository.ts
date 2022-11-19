5bfecd558c8dcc0f7ee50daed112f1f425c48c18
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-18 23:35:32 -0300

    [feat] add OrderRepository

---
+++ src/modules/order/repositories/OrderRepository.ts
@@ -0,0 +1,56 @@
+import { Order } from '@/entities/Order';
+import { MongoOrder } from '@/database/mongodbModels';
+import { iOrderRepository } from './iOrderRepository';
+import { CreateOrderDto } from '../dto/createOrder.dto';
+import { EditOrderDto } from '../dto/editOrderDto';
+import { Product } from '@/entities/Product';
+
+interface OrderListWithProducts {
+  _id?: string;
+  table: string;
+  status?: string;
+  products: { product: string | Product; quantity: number }[];
+}
+export class OrderRepository implements iOrderRepository {
+  async create(dto: CreateOrderDto) {
+    const newOrder = await MongoOrder.create(dto);
+    return new Order({
+      id: newOrder._id.toString(),
+      table: newOrder.table,
+      status: newOrder.status,
+      products: newOrder.products.map(({ product, quantity }) => {
+        return {
+          product: product._id.toString(),
+          quantity: quantity,
+        };
+      }),
+    });
+  }
+
+  async findAll() {
+    const orderList: OrderListWithProducts[] = await MongoOrder.find()
+      .populate('products.product')
+      .lean();
+
+    const cleanedOrderList = orderList.map((order) => {
+      return new Order({
+        id: order._id?.toString(),
+        table: order.table,
+        status: order.status,
+        products: order.products,
+      });
+    });
+
+    return cleanedOrderList;
+  }
+
+  async edit(orderId: string, dto: EditOrderDto) {
+    await MongoOrder.findByIdAndUpdate(orderId, {
+      dto,
+    });
+  }
+
+  async delete(orderId: string) {
+    await MongoOrder.findByIdAndDelete(orderId);
+  }
+}
